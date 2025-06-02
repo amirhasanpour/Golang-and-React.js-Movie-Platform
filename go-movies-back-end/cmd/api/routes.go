f38e66dd70b209d881e15093df2adb5b66f78b69
amirhp <amirhasanpour.hp10@gmail.com>
2025-06-02 22:22:49 +0330

    Create handler to insert a movie

--- go-movies-back-end/cmd/api/routes.go
+++ go-movies-back-end/cmd/api/routes.go
@@ -30,6 +30,7 @@ func (app *application) routes() http.Handler {
 
 		mux.Get("/movies", app.MovieCatalogue)
 		mux.Get("/movies/{id}", app.MovieForEdit)
+		mux.Put("/movies/0", app.InsertMovie)
 	})
 
 	return mux
