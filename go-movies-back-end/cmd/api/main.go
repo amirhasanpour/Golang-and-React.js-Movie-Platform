1a5247fbd4326cee9cbeabaa970b3626f6b4cec3
amirhp <amirhasanpour.hp10@gmail.com>
2025-06-03 00:16:16 +0330

    Get movie image from remote api

--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -21,6 +21,7 @@ type application struct {
 	JWTIssuer    string
 	JWTAudience  string
 	CookieDomain string
+	APIKey 		 string
 }
 
 func main() {
@@ -34,6 +35,7 @@ func main() {
 	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
 	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
 	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
+	flag.StringVar(&app.APIKey, "api-key", "9c89eaa00a4a6eefe56f5f53fe3b05a8", "api key")
 	flag.Parse()
 
 	// connect to the database
